a5d33c9c3985850ff625327c5333a0607fb681db
Fer Tavera <fer8707@gmail.com>
2025-12-16 16:50:17 -0600

    ejemplo de git

--- 11-redux-toolkit-tutorial/final/src/App.js
+++ 11-redux-toolkit-tutorial/final/src/App.js
@@ -5,6 +5,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 import { useEffect } from 'react';
 import Modal from './components/Modal';
+
+
+
 function App() {
   const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
